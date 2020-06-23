0e0bf41b36bc7c7351d624a1de02e6cafdf20d00
Kristin <sj.kristinwei@gmail.com>
2020-06-23 16:01:17 -0400

    done check ingredients validity

--- testApi.py
+++ testApi.py
@@ -23,7 +23,10 @@ class Ingredient(db.Model):
 #     id = db.Column(db.Integer, primary_key=True)
 
 
-    
+class nofoundError(Exception):
+    pass
+
+
 @app.route('/')
 def index():
     db.session.query(Ingredient).delete()
@@ -61,49 +64,69 @@ def ingredients():
 # add ingredients routes
 # ===========================
 # 1. check repeated ingredients (DONE)
-# 2. check typo
-# 3. check no result found case
+# 2. check typo (DONE)
+# 3. check no result found case (DONE)
+
+
 
 @app.route('/addpro', methods=['POST'])
 def addpro():
-
-
     try:
         added = request.form['protein']
         apiID = '1ddd0896'
         apiKEY = '58ef01156cda25a59462f34755cb565d'
-        proURL = "https://api.edamam.com/search?app_id={0}&app_key={1}&q={2}".format(apiID, apiKEY, added)
-
+        addURL = "https://api.edamam.com/search?app_id={0}&app_key={1}&q={2}".format(apiID, apiKEY, added)
         # typo & no found check
-        foundNum = int(requests.get(proURL).json()['count'])
-        assert foundNum > 0
-
+        foundNum = int(requests.get(addURL).json()['count'])
+        if foundNum < 1:
+            raise nofoundError
         # repeated check
         ingredient = Ingredient(type="pro", name=request.form['protein'])
         db.session.add(ingredient)
         db.session.commit()
-    except exc.SQLAlchemyError as e:
-        flash('{} has already been added!'.format(added))
-        # print(type(e))
-    except:
+
+    except nofoundError:
         flash("{} is not a valid input, plase check spelling or pick other protein".format(added))
 
-    return redirect(url_for('ingredients'))
+    except exc.SQLAlchemyError as e:
+        flash('{} has already been added!'.format(added))
+    
+    except:
+        pass
+
+    finally:
+        return redirect(url_for('ingredients'))
 
 
 
 @app.route('/addveg', methods=['POST'])
 def addveg():
     try:
-        added = request.form['vagetables']
-        ingredient = Ingredient(type="veg", name=request.form['vagetables'])
+        added = request.form['veg']
+        print(added)
+        apiID = '1ddd0896'
+        apiKEY = '58ef01156cda25a59462f34755cb565d'
+        addURL = "https://api.edamam.com/search?app_id={0}&app_key={1}&q={2}".format(apiID, apiKEY, added)
+        # typo & no found check
+        foundNum = int(requests.get(addURL).json()['count'])
+        if foundNum == 0:
+            raise nofoundError
+        # repeated check
+        ingredient = Ingredient(type="veg", name=request.form['veg'])
         db.session.add(ingredient)
         db.session.commit()
+
+    except nofoundError:
+        flash("{} is not a valid input, plase check spelling or pick other vegetables".format(added))
+
     except exc.SQLAlchemyError as e:
-        flash('{} has been added!'.format(added))
-        print(type(e))
+        flash('{} has already been added!'.format(added))
     
-    return redirect(url_for('ingredients'))
+    except:
+        pass
+
+    finally:
+        return redirect(url_for('ingredients'))
 
 
 
@@ -111,14 +134,29 @@ def addveg():
 def addcarb():
     try:
         added = request.form['carbohydrates']
+        apiID = '1ddd0896'
+        apiKEY = '58ef01156cda25a59462f34755cb565d'
+        addURL = "https://api.edamam.com/search?app_id={0}&app_key={1}&q={2}".format(apiID, apiKEY, added)
+        # typo & no found check
+        foundNum = int(requests.get(addURL).json()['count'])
+        if foundNum < 1:
+            raise nofoundError
+        # repeated check
         ingredient = Ingredient(type="carb", name=request.form['carbohydrates'])
         db.session.add(ingredient)
         db.session.commit()
-    except IntegrityError:
-        flash('{} has been added!'.format(added))
-        db.session.rollback()
 
-    return redirect(url_for('ingredients'))
+    except nofoundError:
+        flash("{} is not a valid input, plase check spelling or pick other carbohydrates".format(added))
+
+    except exc.SQLAlchemyError as e:
+        flash('{} has already been added!'.format(added))
+    
+    except:
+        pass
+
+    finally:
+        return redirect(url_for('ingredients'))
 
 
 
